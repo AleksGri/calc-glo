0a50363ae9bfd28e183bd08fecbac7d06750f64d
AleksGri <aleks032118@gmail.com>
2021-07-30 15:19:29 -0700

    FIX(script.js): change Initialization to allocation

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,4 @@
-const money = 'US Dollars'; 
-let income = 95000; 
-let addExpenses = 1500; 
-let deposit = 'Bi-weekly'; 
-const mission = 'Saving';
-let period = 398;
-alert('My annual income is ' + income + ' of '+ money); 
+let money, income, addExpenses, deposit, mission;
+let period;
+alert('Alert message'); 
+console.log('Console message');
