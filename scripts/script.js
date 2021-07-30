7b7442f7fe99acbfd4e95cdc48e7d3d39e17400e
AleksGri <aleks032118@gmail.com>
2021-07-29 23:10:03 -0700

    FEAT(script.js): add variables

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const money = 'US Dollars'; 
+let income = 95000; 
+let addExpenses = 1500; 
+let deposit = 'Bi-weekly'; 
+const mission = 'Saving';
+let period = 398;
+alert('My annual income is ' + income + ' of '+ money); 
