09c746e91e291a95e054589b704ef908d48b06b3
AleksGri <aleks032118@gmail.com>
2021-07-31 09:29:57 -0700

    FIX(script.js): change variable declaration

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,29 @@
-let money, income, addExpenses, deposit, mission;
-let period;
-
+'use strict';
 alert('Alert message');
 
 console.log('Console message');
 
 // Lesson02
 // 1)
-money = 16000;
-income = 'Uber';
-addExpenses = 'Internet, Taxi, Utilities';
-deposit = true;
-mission = 10e6;
-period = 12;
+let money = 16000;
+let income = 'Uber';
+let addExpenses = 'Internet, Taxi, Utilities';
+let deposit = true;
+const mission = 10e6;
+let period = 12;
+let budgetDay;
 
 //2)
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money + ' ' + typeof income + ' ' + typeof deposit);
 
 console.log(addExpenses.length);
 
 console.log(`Период равен ${period} месяцев`);
 console.log('Цель заработать ' + mission + ' долларов');
 
-let expensesArray = addExpenses.toLowerCase().split(', ');
-console.log(expensesArray);
+console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = money / 30;
+budgetDay = money / 30;
 console.log(budgetDay);
 
 
